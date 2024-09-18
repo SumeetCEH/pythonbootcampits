a2bb66f7b52756723ebe47a3e9a09fe2b63c8051
Sumeet Kumar <Sraj2784kumar@gmail.com>
2024-09-18 12:55:27 +0530

    sumeeettt

---
+++ matplotlibdemo.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as java
+
+x = [60,71,72,73.13]
+y = [2018,2019,2020,2024]
+
+java.pie(x)
+java.title("Sumeet's Graph")
+java.xlabel("Sumeet's PERFORMANCE IN %")
+java.ylabel("MARKS FROM 10TH TO GRADUATION [BCA]")
+java.plot(y,x)
+java.show()
